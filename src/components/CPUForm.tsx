c3d4b0698970d208f40b3cc5fc768a62b60c39ce
needhourger <chainyuuki@gmail.com>
2024-02-08 17:55:24 +0800

    feat: remove old cpu form

--- src/components/CPUForm.tsx
+++ src/components/CPUForm.tsx
@@ -1,4 +1,4 @@
-import { Form, Input, Select } from "antd";
+import { Form, Input, InputNumber, Select } from "antd";
 import FormItem from "antd/es/form/FormItem";
 import { cpuManufacturer } from "@/libs/data";
 
@@ -7,22 +7,5 @@ interface Props {
 }
 
 export default function CPUForm(props: Props) {
-  return (
-    <div className="flex items-center">
-      <h2 className="me-2">
-        CPU <span className=" rounded-sm bg-green-600 px-1">{props.no}</span>
-      </h2>
-      <Form layout="inline" variant="filled">
-        <FormItem label="Manufacturer">
-          <Select
-            placeholder="Select a manufacturer"
-            options={cpuManufacturer}
-            style={{ width: 200 }}></Select>
-        </FormItem>
-        <FormItem label="Model">
-          <Input />
-        </FormItem>
-      </Form>
-    </div>
-  );
+  return <h1>CPU</h1>;
 }
--- src/components/MotherBoardForm.tsx
+++ src/components/MotherBoardForm.tsx
@@ -1,12 +1,43 @@
-import { Form, Select } from "antd";
+import { Col, Form, Input, InputNumber, Row, Select } from "antd";
 import FormItem from "antd/es/form/FormItem";
+import CPUForm from "./CPUForm";
+import { cpuManufacturer } from "@/libs/data";
 
 export default function MotherBoardForm() {
   return (
-    <Form>
-      <FormItem label="Socket Type" name="socketType">
-        <Select></Select>
-      </FormItem>
-    </Form>
+    <div>
+      <h1>Mother Board</h1>
+      <Form labelCol={{ span: 6 }}>
+        <Row gutter={0}>
+          <Col span={6}>
+            <FormItem label="Manufacturer" name="manufacturer">
+              <Select
+                options={cpuManufacturer}
+                style={{ width: "100%" }}></Select>
+            </FormItem>
+          </Col>
+          <Col span={6}>
+            <FormItem label="Socket" name="socket">
+              <Select></Select>
+            </FormItem>
+          </Col>
+          <Col span={6}>
+            <FormItem label="Chipset" name="chipset">
+              <Select></Select>
+            </FormItem>
+          </Col>
+          <Col span={6}>
+            <FormItem label="Price" name="price">
+              <InputNumber min={0} addonBefore="$" />
+            </FormItem>
+          </Col>
+          <Col span={18}>
+            <FormItem label="Model" name="model" labelCol={{ span: 2 }}>
+              <Input />
+            </FormItem>
+          </Col>
+        </Row>
+      </Form>
+    </div>
   );
 }
